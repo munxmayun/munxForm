e056a7b147798efacbb59b744452bbbff9b7e5ce
munxmayun@163.com <317083802@qq.com>
2018-11-30 23:03:17 +0800

    munx

---
+++ src/Form.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace munxForm;
+
+class Form {
+
+    public static function index($message = 'hello') {
+        return $message;
+    }
+
+    public static function getValue($table, $where = null, $value = null) {
+        return 'ceshi';
+    }
+
+}
